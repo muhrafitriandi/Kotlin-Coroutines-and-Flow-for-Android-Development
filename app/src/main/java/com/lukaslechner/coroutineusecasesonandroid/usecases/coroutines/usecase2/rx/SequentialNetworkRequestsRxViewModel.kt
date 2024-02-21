500e3bd634be6ef45f4ebfa337478d5fc87d1ad4
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-02-21 07:47:11 +0700

    RxJava Implementation
    - Consuming sequential network
    - Handling memory leak

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
@@ -1,12 +1,45 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2.rx
 
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 
 class SequentialNetworkRequestsRxViewModel(
     private val mockApi: RxMockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
-    fun perform2SequentialNetworkRequest() {
+    private var disposables = CompositeDisposable()
 
+    fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+
+        mockApi.getRecentAndroidVersions()
+            .flatMap { androidVersions ->
+                val androidVersion = androidVersions.last()
+                mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = { versionFeatures ->
+                    Timber.d("Observe Data Running on: ${Thread.currentThread().name}")
+                    uiState.value = UiState.Success(versionFeatures)
+                },
+                onError = {
+                    uiState.value = UiState.Error(it.message.toString())
+                }
+            ).addTo(disposables)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        Timber.d("Status Disposables Before Clear: ${disposables.isDisposed}")
+        disposables.dispose()
+        Timber.d("Status Disposables After Clear: ${disposables.isDisposed}")
     }
 }
