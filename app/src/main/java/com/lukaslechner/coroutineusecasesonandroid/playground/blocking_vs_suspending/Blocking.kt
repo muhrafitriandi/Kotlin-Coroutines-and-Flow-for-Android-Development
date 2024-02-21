1bf7a19cc56ec6b53770e4b1b8e10a0958017033
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-02-21 18:51:24 +0700

    Blocking vs Suspending

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/blocking_vs_suspending/Blocking.kt
@@ -0,0 +1,25 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.blocking_vs_suspending
+
+import kotlin.concurrent.thread
+
+fun main() {
+    println("Program started")
+    routines(1, 1000)
+    routines(2, 500)
+    thread {
+        repeat(5) {
+            println("${it + 1} || ${Thread.currentThread().name}")
+            Thread.sleep(250)
+        }
+    }
+    Thread.sleep(1500)
+    println("Program finished")
+}
+
+fun routines(number: Int, delay: Long) {
+    thread {
+        println("Routines start $number || ${Thread.currentThread().name}")
+        Thread.sleep(delay)
+        println("Routines finished $number || ${Thread.currentThread().name}")
+    }
+}
