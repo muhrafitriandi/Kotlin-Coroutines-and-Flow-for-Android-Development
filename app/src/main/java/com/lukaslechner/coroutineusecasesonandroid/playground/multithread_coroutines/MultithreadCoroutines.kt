b9161ffc29235b4f28e67d468a5c303164681513
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-02-22 13:34:50 +0700

    Multithread Coroutines

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/multithread_coroutines/MultithreadCoroutines.kt
@@ -0,0 +1,24 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.multithread_coroutines
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+fun main() = runBlocking {
+    println("Program started")
+    val job1 = launch { coroutines(1, 1000) }
+    val job2 = launch { coroutines(2, 500) }
+    joinAll(job1, job2)
+    println("Program finished")
+}
+
+suspend fun coroutines(number: Int, delay: Long) {
+    println("Coroutines start $number || ${Thread.currentThread().name}")
+    delay(delay)
+    withContext(Dispatchers.Default) {
+        println("Coroutines finished $number || ${Thread.currentThread().name}")
+    }
+}
