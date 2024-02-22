2adb49784e75914cca819714e16e493eae382a9d
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-02-22 20:48:23 +0700

    Coroutine Builders: Launch and RunBlocking
    - main-safety
    - launch and runBlocking
    - handling error
    - exercise 1 my solution

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
@@ -1,13 +1,28 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class PerformSingleNetworkRequestViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
-    fun performSingleNetworkRequest() {
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        uiState.value = UiState.Error("Error: ${throwable.message}")
+    }
 
+    fun performSingleNetworkRequest() {
+        uiState.value = UiState.Loading
+
+        viewModelScope.launch(handler) {
+            val androidVersion = mockApi.getRecentAndroidVersions()
+            uiState.value = UiState.Success(androidVersion)
+        }
     }
 }
