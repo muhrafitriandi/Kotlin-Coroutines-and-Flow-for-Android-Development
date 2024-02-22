1c6eb97ec9aa6378abac63fd4e4e6c4646d2f8a8
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-02-22 21:40:25 +0700

    Implementing UseCase#3 in a sequential way
    - Consuming sequential network requests for each anroidVersion
    - Handling exception

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -1,14 +1,29 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 
 class PerformNetworkRequestsConcurrentlyViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
-    fun performNetworkRequestsSequentially() {
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        uiState.value = UiState.Error("Error: ${throwable.message}")
+    }
 
+    fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
+
+        viewModelScope.launch(handler) {
+            val androidOreo = mockApi.getAndroidVersionFeatures(27)
+            val androidPie = mockApi.getAndroidVersionFeatures(28)
+            val android10 = mockApi.getAndroidVersionFeatures(29)
+
+            uiState.value = UiState.Success(listOf(androidOreo, androidPie, android10))
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
