b3f00c47ea1717b5410c72d5221ae1f78b2aca5f
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-02-22 23:57:08 +0700

    Exercise 2: Implementing UseCase#4 in a concurrent way
    - Consuming network concurrently
    - Handling exception without try-catch manual.
    - Solve Exercise 2: with My Solution

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
@@ -1,17 +1,43 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 
 class VariableAmountOfNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
-    fun performNetworkRequestsSequentially() {
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        uiState.value = UiState.Error("Error: ${throwable.message}")
+    }
 
+    fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
+
+        viewModelScope.launch(handler) {
+            val versionFeatures = mockApi.getRecentAndroidVersions().map { androidVersion ->
+                mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+            }
+
+            uiState.value = UiState.Success(versionFeatures)
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch(handler) {
+            val versionFeatures = mockApi.getRecentAndroidVersions().map { androidVersion ->
+                async {
+                    mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                }//.await() // will not concurrently
+            }.awaitAll()
+            uiState.value = UiState.Success(versionFeatures)
+        }
     }
 }
