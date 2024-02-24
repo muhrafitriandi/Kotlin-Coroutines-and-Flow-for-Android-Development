75e8d94b33e04af56f21bf6e86355acd0ecde05d
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-02-24 10:32:32 +0700

    Implementing a timeout
    `withTimeout` throw an exception and stop coroutine.
    `withTimeoutOrNull` return null.

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModel.kt
@@ -1,14 +1,53 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase5
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 
 class NetworkRequestWithTimeoutViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
-    fun performNetworkRequest(timeout: Long) {
-
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        uiState.value = UiState.Error("Error: ${throwable.message}")
     }
 
+    fun performNetworkRequest(timeout: Long) {
+        uiState.value = UiState.Loading
+
+        // usingWithTimeOut(timeout)
+        usingWithTimeOutOrNull(timeout)
+    }
+
+    private fun usingWithTimeOut(timeout: Long) {
+        viewModelScope.launch(handler) {
+            try {
+                val androidVersion = withTimeout(timeout) {
+                    api.getRecentAndroidVersions()
+                }
+                uiState.value = UiState.Success(androidVersion)
+            } catch (e: TimeoutCancellationException) {
+                uiState.value = UiState.Error("Request time out!")
+            }
+        }
+    }
+
+    private fun usingWithTimeOutOrNull(timeout: Long) {
+        viewModelScope.launch(handler) {
+            val androidVersion = withTimeoutOrNull(timeout) {
+                api.getRecentAndroidVersions()
+            }
+
+            if (androidVersion != null) {
+                uiState.value = UiState.Success(androidVersion)
+            } else {
+                uiState.value = UiState.Error("Request time out!")
+            }
+        }
+    }
 }
