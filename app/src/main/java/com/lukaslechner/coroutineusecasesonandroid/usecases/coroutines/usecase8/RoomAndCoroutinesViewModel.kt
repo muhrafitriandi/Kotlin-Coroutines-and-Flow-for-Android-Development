2b49639a40d443e37d77791ce64ef6df954b0221
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-02-28 22:24:08 +0700

    Using Room with Coroutines
    - Consuming data from the network for first time.
    - Consuming data from the database for the second, third, ... time (offline-first/caching).
    - Handling exception without try-catch manual.

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
@@ -1,19 +1,44 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase8
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 
 class RoomAndCoroutinesViewModel(
     private val api: MockApi,
     private val database: AndroidVersionDao
 ) : BaseViewModel<UiState>() {
 
-    fun loadData() {
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        uiState.value = UiState.Error(DataSource.NETWORK, "${throwable.message}")
+    }
 
+    fun loadData() {
+        uiState.value = UiState.Loading.LoadFromDb
+
+        viewModelScope.launch(handler) {
+            val localVersions = database.getAndroidVersions()
+            if (localVersions.isEmpty()) {
+                uiState.value = UiState.Error(DataSource.DATABASE, "Database Empty!")
+            } else {
+                uiState.value = UiState.Success(DataSource.DATABASE, localVersions.mapToUiModelList())
+            }
+
+            uiState.value = UiState.Loading.LoadFromNetwork
+            val networkVersions = api.getRecentAndroidVersions()
+            networkVersions.forEach { androidVersion ->
+                database.insert(androidVersion.mapToEntity())
+            }
+            uiState.value = UiState.Success(DataSource.NETWORK, networkVersions)
+        }
     }
 
     fun clearDatabase() {
-
+        viewModelScope.launch(handler) {
+            database.clear()
+        }
     }
 }
 
