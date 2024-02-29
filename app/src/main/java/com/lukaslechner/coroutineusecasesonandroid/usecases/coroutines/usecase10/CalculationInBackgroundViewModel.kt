ae295e7de2ad318342f1ccfba34cde71f2f381e3
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-02-29 20:23:57 +0700

    Implementation running on Main Thread
    Performing heavy operations, such as calculating a factorial for a large number and then converting it into a specific data type, which takes a long time, should not be done on the main thread. This could cause the UI to freeze.

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -1,10 +1,34 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase10
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.launch
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
 
     fun performCalculation(factorialOf: Int) {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            var result: BigInteger
+            val computationDuration = measureTimeMillis {
+                result = calculateFactorialOf(factorialOf)
+            }
+
+            var resultString: String
+            val stringComputationDuration = measureTimeMillis {
+                resultString = result.toString()
+            }
+
+            uiState.value = UiState.Success(resultString, computationDuration, stringComputationDuration)
+        }
+    }
+
+    private fun calculateFactorialOf(number: Int) = (1..number).fold(
+        BigInteger.ONE
+    ) { acc, i ->
+        acc.multiply(BigInteger.valueOf(i.toLong()))
     }
 }
