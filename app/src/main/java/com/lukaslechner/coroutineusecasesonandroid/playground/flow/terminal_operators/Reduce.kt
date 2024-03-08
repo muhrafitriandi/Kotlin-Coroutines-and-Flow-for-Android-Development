465b53d60a13b48dc15419f42d367e3e8a0c5e35
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-03-09 00:44:41 +0700

    Basic Terminal operators
    - Flow flow{} vs List buildList{}
    - collect{}
    - first{}
    - last()
    - single()
    - toList() and toSet()
    - fold() and reduce()

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/terminal_operators/Reduce.kt
@@ -0,0 +1,29 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.terminal_operators
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.reduce
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+
+    val flow = flow {
+        delay(100)
+
+        println("Emitting first value")
+        emit(1)
+
+        delay(100)
+
+        println("Emitting second value")
+        emit(2)
+    }
+
+    runBlocking {
+        val item = flow.reduce { accumulator, emittedItem ->
+            println("$accumulator + $emittedItem = ${accumulator + emittedItem}")
+            accumulator + emittedItem
+        }
+        println("Received $item")
+    }
+}
