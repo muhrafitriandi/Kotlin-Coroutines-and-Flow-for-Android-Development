ee1cd8d06bb672885ae07773ea45bdee4086ae1b
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-03-09 03:45:11 +0700

    Using "launchIn()" in UseCase1
    Collect data from flow with simple way (concurrent) without manual (sequential).

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -14,11 +16,20 @@ class FlowUseCase1ViewModel(
     val currentStockPriceAsLiveData: LiveData<UiState> = MutableLiveData()
 
     init {
-        viewModelScope.launch {
-            stockPriceDataSource.latestStockList.collect { stockList ->
+        // Simple
+        stockPriceDataSource.latestStockList
+            .onEach { stockList ->
                 Timber.d("Received item: ${stockList.first()}")
                 uiState.value = UiState.Success(stockList)
             }
-        }
+            .launchIn(viewModelScope)
+
+        // Manual
+//        viewModelScope.launch {
+//            stockPriceDataSource.latestStockList.collect { stockList ->
+//                Timber.d("Received item: ${stockList.first()}")
+//                uiState.value = UiState.Success(stockList)
+//            }
+//        }
     }
 }
