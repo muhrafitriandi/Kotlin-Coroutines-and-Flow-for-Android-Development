cd61547f5559d64341905aa23e452e860513a364
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-03-09 22:09:02 +0700

    Lifecycle Operators
    - Show loading indicator before collect data in flow

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase1/FlowUseCase1ViewModel.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -18,9 +21,18 @@ class FlowUseCase1ViewModel(
     init {
         // Simple
         stockPriceDataSource.latestStockList
-            .onEach { stockList ->
-                Timber.d("Received item: ${stockList.first()}")
-                uiState.value = UiState.Success(stockList)
+            .map { stockList ->
+                UiState.Success(stockList) as UiState
+            }
+            .onStart {
+                emit(UiState.Loading)
+                Timber.d("The flow started!")
+            }
+            .onCompletion {
+                Timber.d("The flow completed!")
+            }
+            .onEach {
+                uiState.value = it
             }
             .launchIn(viewModelScope)
 
