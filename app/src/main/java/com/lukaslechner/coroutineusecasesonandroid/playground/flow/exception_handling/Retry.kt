6788a9e1bd3b1de25ec3838c673d7b26b695d5fc
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-03-10 21:19:10 +0700

    The “retry()” operator
    `retryWhen` - Flexible, will retry based on certain conditions.
    `retry()` - Fixed or limited retry.

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/flow/exception_handling/Retry.kt
@@ -0,0 +1,41 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.flow.exception_handling
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.retryWhen
+import kotlinx.coroutines.launch
+
+suspend fun main(): Unit = coroutineScope {
+
+    launch {
+        stockFlow()
+            .catch { throwable ->
+                println("Handle exception in catch() operator $throwable")
+            }
+            .collect { stockData ->
+                println("Collected $stockData")
+            }
+    }
+}
+
+fun stockFlow(): Flow<String> = flow {
+    repeat(5) { index ->
+
+        delay(1000) // Network call
+
+        if (index < 4) {
+            emit("New stock data")
+        } else {
+            throw NetworkException("Network Request Failed!")
+        }
+    }
+}.retryWhen { cause, attempt ->
+    println("Enter retry() with $cause")
+    delay(1000 * (attempt + 1))
+    cause is NetworkException
+}
+
+class NetworkException(message: String) : Exception(message)
